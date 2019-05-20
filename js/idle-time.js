49b87e05a731722c62b1b024c4d8e698c6fb28eb
Desislava Genova <desislava@truelayer.com>
2019-05-20 14:38:41 +0100

    first commit

---
+++ js/idle-time.js
@@ -0,0 +1,17 @@
+        var idleTime = function () {
+            var time;
+            window.onload = resetTimer;
+            document.onmousemove = resetTimer;
+            document.onkeypress = resetTimer;
+
+            function logout() {
+                window.location.href = "../index.html"
+            }
+
+            function resetTimer() {
+                clearTimeout(time);
+                time = setTimeout(logout, 115000);
+            }
+        };
+
+        idleTime();
